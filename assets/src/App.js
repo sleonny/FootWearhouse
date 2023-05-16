c7c33c9cf8cfca8221e70e9e9e8a2ef46d90c347
New <new@News-MacBook-Pro.local>
2023-05-16 19:50:09 -0400

    installed react router dom and added log in and sign uop routes

--- assets/src/App.js
+++ assets/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import SignUp from "./SignUp";
+import LogIn from "./LogIn";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Login</Link>
+            </li>
+            <li>
+              <Link to="/sign-up">Sign up</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Routes> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/sign-up" element={<SignUp/>} />
+          <Route path="/" element={<LogIn/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ assets/src/LogIn.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LogIn = () => {
+    return (
+        <div> Log in </div>
+    )
+};
+
+export default LogIn;
