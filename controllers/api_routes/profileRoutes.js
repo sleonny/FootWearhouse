ef329b70a61ed2443674b80e444e47889e85c04d
Sean L <leonardmsean@gmail.com>
2023-05-16 20:12:33 -0400

    added right const and paths to profileRoutes.js

--- controllers/api_routes/profileRoutes.js
+++ controllers/api_routes/profileRoutes.js
@@ -1,6 +1,6 @@
-const express = require("express");
 const router = express.Router();
-const { Profile } = require("./profileRoutes");
+const { Profile } = require("../api_routes/index");
+
 
 // Get all profiles
 router.get("/", async (req, res) => {
