795c772fda661110b0a94868cda2c1fc0b041e60
Patrick Morgan <patrickdmorgan2@gmail.com>
2023-05-25 17:26:10 -0400

    removed carousel captions

--- assets/src/App.jsx
+++ assets/src/App.jsx
@@ -56,10 +56,6 @@ const Home = () => {
           src={sports}
           alt="Sport Shoes"
         />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
@@ -67,11 +63,6 @@ const Home = () => {
           src={stretchShoes}
           alt="Woman Stretching"
         />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
@@ -79,13 +70,6 @@ const Home = () => {
           src={heels}
           alt="Heels"
         />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
       </Carousel.Item>
       </Carousel>
     </div>
