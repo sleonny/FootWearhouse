78e53d2b54c167c551dcfc81bda5d90920a23057
Patrick Morgan <patrickdmorgan2@gmail.com>
2023-05-25 11:17:53 -0400

    modify shoe.jsx to use handlebars

--- assets/src/LogIn.jsx
+++ assets/src/LogIn.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Button, Modal } from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const initialFormData = {
     email: "",
--- assets/src/Shoe.jsx
+++ assets/src/Shoe.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Handlebars from 'handlebars/dist/handlebars';
+
+const hbr = `
+<h2>{{name}}</h2>
+<p>Description: {{description}}</p>
+<p>This listing is for a size {{size}} and is priced at {{price}}.
+`;
+
+const template = Handlebars.compile(hbr);
 
 const Shoe = ({ shoe }) => {
     return (
         <div>
-            Shoe
+            <div dangerouslySetInnerHTML={{ __html: template(shoe) }} />
         </div>
     )
 };
