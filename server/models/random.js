4b1fba83012e5ae6592e05ca26944771bfde96d6
Sean L <leonardmsean@gmail.com>
2023-05-30 12:35:32 -0400

    random

--- server/models/random.js
+++ server/models/random.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
-const User = require("./User");
+const User = require("./something");
 
 class Shoe extends Model {}
 
